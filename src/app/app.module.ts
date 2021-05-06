4a2f4b5ea7f286b9ac16300a8b497c491a0e90f1
Alessandra Varisco <alessandravariscod3@gmail.com>
2021-05-05 23:38:04 -0400

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { SinginComponent } from './singin/singin.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent,
+    ClientesComponent,
+    SinginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
